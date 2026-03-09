302adf9070dbd4c1e2ed6a5b431e572edfe94ac3
bayujo <bayu.johar25@gmail.com>
2026-03-09 13:30:45 +0800

    Initial Commit

---
+++ Flashnote/FlashnoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashnoteApp.swift
+//  Flashnote
+//
+//  Created by Johar Bayu Kurniawan on 09/03/26.
+//
+
+import SwiftUI
+
+@main
+struct FlashnoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
